1894c0cd79e18ee3837241ed7536a2952c167738
Mason Flint <masonflint44@gmail.com>
2024-03-30 22:05:52 +0000

    updated to use my miniforge devcontainer template

--- .scripts/build-environment.sh
+++ .scripts/build-environment.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-MERGED_ENV_FILE=/tmp/merged-environment.yml
+DEV_ENV_NAME=`basename $(pwd)`
+FROZEN_ENV_NAME=$DEV_ENV_NAME-frozen
+
+MERGED_ENV_PATH=/tmp/merged-environment.yml
+DEV_ENV_FILE=environment.dev.yml
+APP_ENV_FILE=environment.yml
+FROZEN_ENV_FILE=environment.lock.yml
 
 # Merge the environment files
-conda-merge environment.dev.yml environment.yml > $MERGED_ENV_FILE
+conda-merge $DEV_ENV_FILE $APP_ENV_FILE > $MERGED_ENV_PATH
 
 # Check if the dev environment exists
-if mamba env list | grep -wq $ENVIRONMENT_NAME-dev; then
+if mamba env list | grep -wq $DEV_ENV_NAME; then
     # Update the dev environment from the merged file
-    mamba env update -f $MERGED_ENV_FILE -n $ENVIRONMENT_NAME-dev
-
-    # Remove unused packages
-    mamba clean --all -y
+    mamba env update -f $MERGED_ENV_PATH -n $DEV_ENV_NAME
 else
     # Create the dev environment from the merged file if it does not exist
-    mamba env create -f $MERGED_ENV_FILE -n $ENVIRONMENT_NAME-dev
+    mamba env create -f $MERGED_ENV_PATH -n $DEV_ENV_NAME
 fi
 
-# Check if the app environment exists
-if mamba env list | grep -wq $ENVIRONMENT_NAME; then
-    # Update the app environment from the merged file
-    mamba env update -f environment.yml -n $ENVIRONMENT_NAME
-
-    # Remove unused packages
-    mamba clean --all -y
+# Check if the frozen environment exists
+if mamba env list | grep -wq $FROZEN_ENV_NAME; then
+    # Update the frozen environment from the merged file
+    mamba env update -f $APP_ENV_FILE -n $FROZEN_ENV_NAME
 else
-    # Create the app environment from the merged file if it does not exist
-    mamba env create -f environment.yml -n $ENVIRONMENT_NAME
+    # Create the frozen environment from the merged file if it does not exist
+    mamba env create -f $APP_ENV_FILE -n $FROZEN_ENV_NAME
 fi
 
-# Export the app environment to a lock file
-mamba env export -n $ENVIRONMENT_NAME > environment.lock.yml
+# Remove unused packages
+mamba clean --all -y
+
+# Export the frozen environment to a lock file
+mamba env export -n $FROZEN_ENV_NAME > $FROZEN_ENV_FILE
--- .scripts/update-bashrc.sh
+++ .scripts/update-bashrc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+DEV_ENV_NAME=`basename $(pwd)`
+
 # Set conda the environment to activate automatically
-echo "mamba activate $ENVIRONMENT_NAME-dev" >> ~/.bashrc
+echo "mamba activate $DEV_ENV_NAME" >> ~/.bashrc
